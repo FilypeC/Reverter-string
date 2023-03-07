5b51f024cd3972806caeed60c8d516f36ce32575
FilypeC <filype.ramos@gmail.com>
2023-03-07 11:05:57 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Informe a palavra a ser invertida:");
+        string entrada = Console.ReadLine();
+        char[] invert = entrada.ToCharArray();
+        int inicio = 0;
+        int final = invert.Length;
+        final = final - 1;
+        char temp;
+
+        while (inicio<= final) {
+        temp = invert[inicio];
+        invert[inicio] = invert[final];
+        invert[final] = temp;
+        inicio++;
+        final--;
+
+        }
+        Console.WriteLine(invert);
+    }
+}
